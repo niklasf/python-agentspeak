cf51ccb96e8df93304e9c63358d9c6a54f45b70e
Niklas Fiekas <niklas.fiekas@backscattering.de>
2016-07-12 21:34:23 +0200

    Add setup.py

---
+++ README.rst
@@ -0,0 +1,13 @@
+pyson - JASON-style AgentSpeak for Python
+=========================================
+
+Setup
+-----
+
+.. code::
+
+    virtualenv -p python3 venv
+    source venv/bin/activate
+
+    pip install -r requirements.txt
+    python setup.py install
---
+++ setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This file is part of the Pyson AgentSpeak interpreter.
+# Copyright (C) 2016 Niklas Fiekas <niklas.fiekas@tu-clausthal.de>.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import setuptools
+import os
+
+
+def read_description():
+    description = open(os.path.join(os.path.dirname(__file__), "README.rst")).read()
+    return description
+
+
+setuptools.setup(
+    name="pyson",
+    version="0.0.1",
+    author="Niklas Fiekas",
+    author_email="niklas.fiekas@tu-clausthal.de",
+    description="JASON-style AgentSpeak for Python.",
+    long_description=read_description(),
+    license="GPL3",
+    keywords="jason multi agent simulation agent-speak",
+    url="https://github.com/niklasf/pyson",
+    packages=["pyson"],
+    install_requires=["colorama"],
+    classifiers=[
+        "Development Status :: 2- Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python 3",
+        "Programming Language :: Python 3.4",
+        "Programming Language :: Python 3.5",
+        "Programming Language :: Python 3 :: Only",
+        "Programming Language :: Other",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Interpreters",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
