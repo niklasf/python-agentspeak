2ca0bcb0c03b83edc371eefdd01712f73ecb6296
Niklas Fiekas <niklas.fiekas@backscattering.de>
2016-09-03 15:42:04 +0200

    Use str instead of repr for variable dump

--- pyson/runtime.py
+++ pyson/runtime.py
@@ -28,7 +28,7 @@ import pyson.parser
 import pyson.lexer
 import pyson.util
 
-from pyson import UnaryOp, BinaryOp, PysonError, pyson_repr
+from pyson import UnaryOp, BinaryOp, PysonError, pyson_str
 
 
 LOGGER = pyson.get_logger(__name__)
@@ -718,7 +718,7 @@ def dump_variables(variables, scope):
 
     for name, variable in sorted(variables.items()):
         if variable in scope:
-            print("%s = %s" % (name, pyson_repr(pyson.deref(variable, scope))))
+            print("%s = %s" % (name, pyson_str(pyson.deref(variable, scope))))
         else:
             not_in_scope.append("%s = %s" % (name, variable))
 
