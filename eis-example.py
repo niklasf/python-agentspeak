ae9a11dc5cc30bdc7b1a64ad184de3f7e95a10ef
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-03-01 15:55:36 +0100

    Add experimental disconnect action

--- eis-example.py
+++ eis-example.py
@@ -3,20 +3,25 @@
 import asyncio
 import pyson.eis
 import pyson.runtime
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
 
-env = pyson.runtime.Environment()
+async def main():
+    env = pyson.runtime.Environment()
 
-with open("agent1.asl") as source:
-    agent1 = env.build_agent(source, pyson.eis.actions, agent_cls=pyson.eis.Agent)
+    with open("agent1.asl") as source:
+        agent1 = env.build_agent(source, pyson.eis.actions, agent_cls=pyson.eis.Agent)
 
-env.run_agent(agent1)
+    await agent1.connect("hello", "there")
 
+    while env.run_agent(agent1):
+        pass
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-
     try:
+        loop.run_until_complete(main())
         loop.run_forever()
     finally:
         loop.close()
--- pyson/eis.py
+++ pyson/eis.py
@@ -7,9 +7,35 @@ import pyson.stdlib
 actions = pyson.Actions(pyson.stdlib.actions)
 
 class Agent(pyson.runtime.Agent, asyncio.Protocol):
+    def __init__(self):
+        super(Agent, self).__init__()
+
     def connect(self, username, password, host="localhost", port=12300):
         self.username = username
         self.password = password
 
         loop = asyncio.get_event_loop()
         return loop.create_connection(lambda: self, host, port)
+
+    @actions.add(".disconnect", 0)
+    def _disconnect(self, term, intention):
+        self.transport.close()
+        print("Closing !!!")
+        yield
+
+    def connection_made(self, transport):
+        self.transport = transport
+
+        self.call(
+            pyson.Trigger.addition,
+            pyson.GoalType.achievement,
+            pyson.Literal("connected"),
+            pyson.runtime.Intention())
+
+    def connection_lost(self, exc):
+        print("Connection lost")
+        self.call(
+            pyson.Trigger.addition,
+            pyson.GoalType.achievement,
+            pyson.Literal("disconnected", (exc, )),
+            pyson.runtime.Intention())
