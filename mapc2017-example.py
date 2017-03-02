7f729bd7b3abf6317ad10b3eba0a88de9ebca40b
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-03-02 15:29:44 +0100

    Create 6 agents

--- mapc2017-example.py
+++ mapc2017-example.py
@@ -6,7 +6,7 @@ import pyson.runtime
 import logging
 
 
-#pyson.get_logger("pyson.mapc2017").setLevel(logging.DEBUG)
+pyson.get_logger("pyson.mapc2017").setLevel(logging.DEBUG)
 
 
 async def main():
@@ -14,8 +14,23 @@ async def main():
 
     with open("agent1.asl") as source:
         agent1 = env.build_agent(source, pyson.mapc2017.actions, agent_cls=pyson.mapc2017.Agent)
+    with open("agent1.asl") as source:
+        agent2 = env.build_agent(source, pyson.mapc2017.actions, agent_cls=pyson.mapc2017.Agent)
+    with open("agent1.asl") as source:
+        agent3 = env.build_agent(source, pyson.mapc2017.actions, agent_cls=pyson.mapc2017.Agent)
+    with open("agent1.asl") as source:
+        agent4 = env.build_agent(source, pyson.mapc2017.actions, agent_cls=pyson.mapc2017.Agent)
+    with open("agent1.asl") as source:
+        agent5 = env.build_agent(source, pyson.mapc2017.actions, agent_cls=pyson.mapc2017.Agent)
+    with open("agent1.asl") as source:
+        agent6 = env.build_agent(source, pyson.mapc2017.actions, agent_cls=pyson.mapc2017.Agent)
 
     await agent1.connect("agentA1", "1")
+    await agent2.connect("agentA2", "1")
+    await agent3.connect("agentA3", "1")
+    await agent4.connect("agentA4", "1")
+    await agent5.connect("agentA5", "1")
+    await agent6.connect("agentA6", "1")
 
 
 if __name__ == "__main__":
