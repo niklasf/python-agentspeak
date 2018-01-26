ca400765a0ef6fe82378f1be9f9698e2af2aacf8
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-01-26 18:34:14 +0100

    encapsulate java code walking in visitor

--- examples/refactorings/snapshot.py
+++ examples/refactorings/snapshot.py
@@ -10,106 +10,111 @@ def merge_declaration(decl, scope):
         scope[declarator.name] = decl.type.name
 
 
-def walk_compilation_unit(unit):
-    for decl in unit.types:
-        if isinstance(decl, javalang.tree.ClassDeclaration):
-            walk_klass(decl)
+class Visitor:
+    def walk_compilation_unit(self, unit):
+        for decl in unit.types:
+            if isinstance(decl, javalang.tree.ClassDeclaration):
+                self.walk_klass(decl)
 
+    def visit_klass(self, klass):
+        print("class(\"%s\")." % klass.name)
 
-def walk_klass(klass):
-    print("class(\"%s\")." % klass.name)
+    def visit_method(self, klass, method):
+        print("method(\"%s\", \"%s\", %d, %d)." % (klass.name, method.name, loc(method), complexity(method)))
 
-    scope = {}
-
-    for field in klass.fields:
-        merge_declaration(field, scope)
-
-    for method in klass.methods:
-        walk_method(klass, method, scope)
-
-
-def walk_method(klass, method, scope):
-    print("method(\"%s\", \"%s\", %d, %d)." % (klass.name, method.name, loc(method), complexity(method)))
-
-    arg_scope = {}
-    for parameter in method.parameters:
-        arg_scope[parameter.name] = parameter.type.name
-
-    scope = collections.ChainMap(arg_scope, scope)
-
-    if method.body:
-        walk_block(klass, method, method.body, scope)
-
-
-def walk_block(klass, method, stmts, scope):
-    scope = collections.ChainMap({}, scope)
-    for stmt in stmts:
-        walk_statement(klass, method, stmt, scope)
-
-
-def walk_statement(klass, method, stmt, scope):
-    if isinstance(stmt, javalang.tree.StatementExpression):
-        walk_expression(klass, method, stmt.expression, scope)
-    elif isinstance(stmt, javalang.tree.IfStatement):
-        walk_expression(klass, method, stmt.condition, scope)
-        walk_statement(klass, method, stmt.then_statement, scope)
-        if stmt.else_statement:
-            walk_statement(klass, method, stmt.else_statement, scope)
-    elif isinstance(stmt, javalang.tree.VariableDeclaration):
-        merge_declaration(stmt, scope)
-    elif isinstance(stmt, javalang.tree.BlockStatement):
-        walk_block(klass, method, stmt.statements, scope)
-    elif isinstance(stmt, (javalang.tree.ReturnStatement, javalang.tree.ThrowStatement)):
-        walk_expression(klass, method, stmt.expression, scope)
-    elif isinstance(stmt, (javalang.tree.WhileStatement, javalang.tree.DoStatement)):
-        walk_expression(klass, method, stmt.condition, scope)
-        walk_statement(klass, method, stmt.body, scope)
-    elif isinstance(stmt, javalang.tree.ForStatement):
-        s = collections.ChainMap({}, scope)
-        if isinstance(stmt.control, javalang.tree.ForControl):
-            if stmt.control.init:
-                merge_declaration(stmt.control.init, s)
-        else:
-            merge_declaration(stmt.control.var, s)
-        walk_statement(klass, method, stmt.body, s)
-    elif isinstance(stmt, (javalang.tree.BreakStatement, javalang.tree.ContinueStatement)):
-        pass
-    elif isinstance(stmt, javalang.tree.SynchronizedStatement):
-        walk_block(klass, method, stmt.block, scope)
-    elif isinstance(stmt, javalang.tree.TryStatement):
-        if stmt.block:
-            walk_block(klass, method, stmt.block, scope)
-        if stmt.catches:
-            for catch in stmt.catches:
-                # Latest Java can catch multiple exception types in one
-                # statement
-                #assert len(catch.parameter.types) == 1
-                s = collections.ChainMap({}, scope)
-                s[catch.parameter.name] = catch.parameter.types[0]
-                walk_block(klass, method, catch.block, s)
-        if stmt.finally_block:
-            walk_block(klass, method, stmt.finally_block, scope)
-    elif isinstance(stmt, javalang.tree.SwitchStatement):
-        walk_expression(klass, method, stmt.expression, scope)
-        for case in stmt.cases:
-            walk_block(klass, method, case.statements, scope)
-    elif isinstance(stmt, javalang.tree.Statement):
-        pass  # lone semicolon
-    else:
-        raise RuntimeError("unknown stmt %s" % stmt)
-
-
-def walk_expression(klass, method, expression, scope):
-    if isinstance(expression, javalang.tree.MethodInvocation):
-        if not expression.qualifier:
-            qualifier = klass.name
-        elif expression.qualifier[0].islower():
-            qualifier = scope.get(expression.qualifier, expression.qualifier)
-        else:
-            qualifier = expression.qualifier
-
+    def visit_calls(self, klass, method, qualifier, member):
         print("calls(\"%s\", \"%s\", \"%s\", \"%s\")." % (
-            klass.name, method.name, qualifier, expression.member))
+            klass.name, method.name, qualifier, member))
+
+    def walk_klass(self, klass):
+        self.visit_klass(klass)
+
+        scope = {}
+
+        for field in klass.fields:
+            merge_declaration(field, scope)
+
+        for method in klass.methods:
+            self.walk_method(klass, method, scope)
+
+    def walk_method(self, klass, method, scope):
+        self.visit_method(klass, method)
+
+        arg_scope = {}
+        for parameter in method.parameters:
+            arg_scope[parameter.name] = parameter.type.name
+
+        scope = collections.ChainMap(arg_scope, scope)
+
+        if method.body:
+            self.walk_block(klass, method, method.body, scope)
+
+    def walk_block(self, klass, method, stmts, scope):
+        scope = collections.ChainMap({}, scope)
+        for stmt in stmts:
+            self.walk_statement(klass, method, stmt, scope)
+
+    def walk_statement(self, klass, method, stmt, scope):
+        if isinstance(stmt, javalang.tree.StatementExpression):
+            self.walk_expression(klass, method, stmt.expression, scope)
+        elif isinstance(stmt, javalang.tree.IfStatement):
+            self.walk_expression(klass, method, stmt.condition, scope)
+            self.walk_statement(klass, method, stmt.then_statement, scope)
+            if stmt.else_statement:
+                self.walk_statement(klass, method, stmt.else_statement, scope)
+        elif isinstance(stmt, javalang.tree.VariableDeclaration):
+            merge_declaration(stmt, scope)
+        elif isinstance(stmt, javalang.tree.BlockStatement):
+            self.walk_block(klass, method, stmt.statements, scope)
+        elif isinstance(stmt, (javalang.tree.ReturnStatement, javalang.tree.ThrowStatement)):
+            self.walk_expression(klass, method, stmt.expression, scope)
+        elif isinstance(stmt, (javalang.tree.WhileStatement, javalang.tree.DoStatement)):
+            self.walk_expression(klass, method, stmt.condition, scope)
+            self.walk_statement(klass, method, stmt.body, scope)
+        elif isinstance(stmt, javalang.tree.ForStatement):
+            s = collections.ChainMap({}, scope)
+            if isinstance(stmt.control, javalang.tree.ForControl):
+                if stmt.control.init:
+                    merge_declaration(stmt.control.init, s)
+            else:
+                merge_declaration(stmt.control.var, s)
+            self.walk_statement(klass, method, stmt.body, s)
+        elif isinstance(stmt, (javalang.tree.BreakStatement, javalang.tree.ContinueStatement)):
+            pass
+        elif isinstance(stmt, javalang.tree.SynchronizedStatement):
+            self.walk_block(klass, method, stmt.block, scope)
+        elif isinstance(stmt, javalang.tree.TryStatement):
+            if stmt.block:
+                self.walk_block(klass, method, stmt.block, scope)
+            if stmt.catches:
+                for catch in stmt.catches:
+                    # Latest Java can catch multiple exception types in one
+                    # statement
+                    #assert len(catch.parameter.types) == 1
+                    s = collections.ChainMap({}, scope)
+                    s[catch.parameter.name] = catch.parameter.types[0]
+                    self.walk_block(klass, method, catch.block, s)
+            if stmt.finally_block:
+                self.walk_block(klass, method, stmt.finally_block, scope)
+        elif isinstance(stmt, javalang.tree.SwitchStatement):
+            self.walk_expression(klass, method, stmt.expression, scope)
+            for case in stmt.cases:
+                self.walk_block(klass, method, case.statements, scope)
+        elif isinstance(stmt, javalang.tree.Statement):
+            pass  # lone semicolon
+        else:
+            raise RuntimeError("unknown stmt %s" % stmt)
+
+    def walk_expression(self, klass, method, expression, scope):
+        if isinstance(expression, javalang.tree.MethodInvocation):
+            if not expression.qualifier:
+                qualifier = klass.name
+            elif expression.qualifier[0].islower():
+                qualifier = scope.get(expression.qualifier, expression.qualifier)
+            else:
+                qualifier = expression.qualifier
+
+            self.visit_calls(klass, method, qualifier, expression.member)
 
 
 def loc(node):
@@ -205,9 +210,10 @@ def complexity(node):
 
 if __name__ == "__main__":
     #for src in glob2.glob(sys.argv[1] + "/src/main/**/*.java"):
+    visitor = Visitor()
     for src in glob2.glob(sys.argv[1] + "/src/**/*.java"):
         print()
         print("#", src)
         print()
         with open(src) as f:
-            walk_compilation_unit(javalang.parse.parse(f.read()))
+            visitor.walk_compilation_unit(javalang.parse.parse(f.read()))
