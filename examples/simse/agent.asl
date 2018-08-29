5cda6163a3f53914417685fb751c1f483ae7b924
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-08-29 14:33:28 +0200

    simse: add agent.asl

--- examples/simse/agent.asl
+++ examples/simse/agent.asl
@@ -1,4 +1,19 @@
+knowledge("Main", 0.8).
+
+!hello_world.
 !hello_world.
 
 +!hello_world <-
-    .print("hello world").
+    .get_issue(Issue);
+    .random(Rand);
+    !handle_issue(Issue, Rand).
+
++!handle_issue(issue(bug, Class, Priority), Rand) : knowledge(Class, Knowledge) <-
+    -+knowledge(Class, Knowledge + 1);
+    if (Knowledge > Rand) {
+        .print("...")
+    }
+    .print("bug", Class, Knowledge).
+
++!handle_issue(issue(feature, Class, Priority)) <-
+    .print("issue", Class).
--- examples/simse/env.py
+++ examples/simse/env.py
@@ -7,14 +7,25 @@ import pyson.runtime
 
 actions = pyson.Actions(pyson.stdlib.actions)
 
+class Issue:
+    def __init__(self):
+        self.type = "bug" # or feature
+        self.klass = "Main" # or None
+        self.priority = 1
+
+    def as_term(self):
+        return pyson.Literal("issue", (pyson.Literal(self.type), self.klass, self.priority), ())
+
 class Environment(pyson.runtime.Environment):
     def __init__(self):
         super().__init__()
-        pass
+        self.issues = [Issue(), Issue()]
 
-    @actions.add_function(".get_issue", ())
-    def get_issue(self):
-        pass
+    @actions.add(".get_issue", 1)
+    def get_issue(self, term, intention):
+        issue = self.env.issues.pop()
+        if pyson.unify(term.args[0], issue.as_term(), intention.scope, intention.stack):
+            yield
 
 env = Environment()
 
