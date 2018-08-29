91e3428451dbf859673390869b3b0e0bb635c7ca
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-08-29 15:22:42 +0200

    simse: submit patch as pr

--- examples/simse/agent.asl
+++ examples/simse/agent.asl
@@ -1,20 +1,28 @@
+#committer.
+
+!hello_world.
 !hello_world.
 !hello_world.
 
 +!hello_world <-
-    .get_issue(Issue);
-    !handle_issue(Issue).
+    (commiter | Type = bug | Type = feature) & .get_issue(issue(Type, Patch, Priority));
+    !handle_issue(issue(Type, Patch, Priority)).
 
-+!handle_issue(issue(bug, Class, Priority)) <-
-    .random(Rand);
-
-    #if (Knowledge > Rand) {
-    #    .print("...")
-    #}
-    #.print("bug", Class, Knowledge).
-
-    .make_patch(Class, Patch);
-    .print(Patch).
++!handle_issue(issue(bug, Patch, Priority)) <-
+    .print(Patch);
+    !submit_patch(Patch, Priority).
 
 +!handle_issue(issue(feature, Class, Priority)) <-
     .print("issue", Class).
+
++!handle_issue(issue(pr, Class, Priority)) : committer <-
+    .print("Handling pull request").
+
++!handle_issue(_).
+
++!submit_patch(Patch, _) : committer <-
+    .commit(Patch).
+
++!submit_patch(Patch, Priority) : not committer <-
+    .print("Sending pr ...");
+    .submit_issue(pr, Patch, Priority).
--- examples/simse/env.py
+++ examples/simse/env.py
@@ -9,12 +9,15 @@ actions = pyson.Actions(pyson.stdlib.actions)
 
 class Issue:
     def __init__(self):
-        self.type = "bug" # or feature
-        self.klass = "Main" # or None
+        self.type = "bug" # or feature, pr
+
+        #self.klass = "Main" # or None
+        self.patch = None
+
         self.priority = 1
 
     def as_term(self):
-        return pyson.Literal("issue", (pyson.Literal(self.type), self.klass, self.priority), ())
+        return pyson.Literal("issue", (pyson.Literal(self.type), self.patch, self.priority), ())
 
 class Patch:
     def __init__(self):
@@ -28,9 +31,10 @@ class Environment(pyson.runtime.Environment):
 
     @actions.add(".get_issue", 1)
     def get_issue(self, term, intention):
-        issue = self.env.issues.pop()
-        if pyson.unify(term.args[0], issue.as_term(), intention.scope, intention.stack):
-            yield
+        for issue in self.env.issues:
+            if pyson.unify(term.args[0], issue.as_term(), intention.scope, intention.stack):
+                self.env.issues.remove(issue)
+                yield
 
     @actions.add(".make_patch", 2)
     def make_patch(self, term, intention):
@@ -42,6 +46,21 @@ class Environment(pyson.runtime.Environment):
 
         if pyson.unify(term.args[1], patch, intention.scope, intention.stack):
             yield
+
+    @actions.add(".commit", 1)
+    def commit(self, term, intention):
+        patch = pyson.grounded(term.args[0], intention.scope)
+        print("Committing patch", patch)
+        yield
+
+    @actions.add(".submit_issue", 3)
+    def submit_issue(self, term, intention):
+        issue = Issue()
+        issue.type = pyson.grounded(term.args[0], intention.scope)
+        issue.patch = pyson.grounded(term.args[1], intention.scope)
+        issue.priority = pyson.grounded(term.args[1], intention.scope)
+        self.env.issues.insert(0, issue)
+        yield
 
 env = Environment()
 
